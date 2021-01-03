8c8063893c322ede03bac084ff5c4a53e2304858
Jan Kammellander <janschwiderik@gmail.com>
2021-01-04 00:26:04 +0100

    IMPROVED🏗: Rechteck 🎚

---
+++ src/jkammellander/Rechteck.java
@@ -0,0 +1,57 @@
+package jkammellander;
+
+public class Rechteck extends MathFigur{
+    // Attribute
+    private double a, b;
+
+    /**
+     * Konstruktor der Klasse
+     * @param a Die Länge des Redchtecks
+     * @param b Die Breite des Rechtecks
+     */
+    public Rechteck(double a, double b) {
+        if (laengenCheck()) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+
+    /**
+     * Diese Methode setzt sinnvolle Standardwerte eines Rechtecks
+     */
+    @Override
+    public void initialisieren() {
+        this.a = 10;
+        this.b = 5;
+    }
+
+    /**
+     * Diese Methode berechnet den Flächeninhalt eines Rechtecks
+     *
+     * @return Der Flächeninhalt eines Rechtecks
+     */
+    @Override
+    public double flaecheBerechnen() {
+        return this.a * this.b;
+    }
+
+    /**
+     * Diese Methode berechnet den Umfang eines Rechtecks
+     *
+     * @return Der Umfang eines Rechtecks
+     */
+    @Override
+    public double umfangBerechnen() {
+        return this.a*2 + this.b*2;
+    }
+
+    /**
+     * Diese Methode prüft die Länge und Breite auf einen gültigen Bereich
+     *
+     * @return true, wenn die Länge und Breite gültigen Bereich liegt
+     */
+    @Override
+    public boolean laengenCheck() {
+        return this.a > 0 && this.b > 0;
+    }
+}
